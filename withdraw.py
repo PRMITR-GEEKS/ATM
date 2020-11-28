c0431355e956b970e06ff6617a6014d7afec75b9
Avinash Kharate <avinashkharate29@gmail.com>
2020-11-28 17:48:36 +0530

    Updated

--- withdraw.py
+++ withdraw.py
@@ -22,6 +22,9 @@ def square(a):
 def sqrt(a):
   return  a**(0.5)
 
+def cbrt(a):
+  return a**(1/3)
+
 a = int(input("Enter The First Number"))
 b = int(input("Enter The Second Number"))
 
@@ -34,4 +37,5 @@ print(f" Modulus of Two Numbers is {mod(a,b)}")
 print(f" Cube Of  Number is {cube(a)}")
 print(f" Square of The Numbers is {square(a)}")
 print(f" Sqrt of The Numbers is {sqrt(a)}")
+print(f" Cube Root of The Numbers is {cbrt(a)}")
 
