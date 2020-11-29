86b7ec19acf8c1f99236824a19ddf9360988321c
Sejal <sejalk1910@gmail.com>
2020-11-28 17:37:02 -0800

    cube added

--- withdraw.py
+++ withdraw.py
@@ -13,6 +13,9 @@ def div(a,b):
 def mod(a,b):
   return a%b
 
+def cube(a):
+  return a**3 
+
 a = int(input("Enter The First Number"))
 b = int(input("Enter The Second Number"))
 
@@ -22,3 +25,4 @@ print(f" Sub Of Two Numbers is {sub(a,b)}")
 print(f" Multiplication Of Two Numbers is {mul(a,b)}")
 print(f" Division Of Two Numbers is {div(a,b)}")
 print(f" Modulus of Two Numbers is {mod(a,b)}")
+print(f" Cube Of  Number is {cube(a)}")
