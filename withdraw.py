191854e5bc6ac54caf8b84f0012daf4f422fce33
Sejal <sejalk1910@gmail.com>
2020-11-28 17:43:58 -0800

    sqrt added

--- withdraw.py
+++ withdraw.py
@@ -13,13 +13,14 @@ def div(a,b):
 def mod(a,b):
   return a%b
 
-<<<<<<< HEAD
 def cube(a):
   return a**3 
-=======
+
 def square(a):
   return a*a
->>>>>>> 0a937959b832dce141c1329427e04b6f92b1ded6
+
+def sqrt(a):
+  return  a**(0.5)
 
 a = int(input("Enter The First Number"))
 b = int(input("Enter The Second Number"))
@@ -30,8 +31,7 @@ print(f" Sub Of Two Numbers is {sub(a,b)}")
 print(f" Multiplication Of Two Numbers is {mul(a,b)}")
 print(f" Division Of Two Numbers is {div(a,b)}")
 print(f" Modulus of Two Numbers is {mod(a,b)}")
-<<<<<<< HEAD
 print(f" Cube Of  Number is {cube(a)}")
-=======
 print(f" Square of The Numbers is {square(a)}")
->>>>>>> 0a937959b832dce141c1329427e04b6f92b1ded6
+print(f" Sqrt of The Numbers is {sqrt(a)}")
+
